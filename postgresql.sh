d5477cb4c1f4f5abde64feedb279f229f3c24a7f
julienh1 <58294367+julienh1@users.noreply.github.com>
2021-01-31 18:03:27 +0100

    Delete postgresql.sh

--- postgresql.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bsh
-
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF.asc | sudo apt-key add -
-echo "deb http://apt.postgresql.org/pub/repos/apt/${RELEASE" -pgdg main | sudo tee /etc/apt/sources.list.d/pgdg.list
-cat /etc/apt/sources.list.d/pgdg.list
-deb http://apt.postgresql.org/pub/repos/apt/ bionic-pgdg main
-sudo apt-get update
-sudo apt -y install postgresql-client-11
