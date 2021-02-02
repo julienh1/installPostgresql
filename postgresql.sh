399eda5e942e68646ef1cebec8d2a41f2e52cb95
julienh1 <58294367+julienh1@users.noreply.github.com>
2021-02-02 07:06:34 +0100

    Update postgresql.sh

--- postgresql.sh
+++ postgresql.sh
@@ -2,7 +2,7 @@
 apt-get install curl ca-certificates gnupg
 curl https://www.postgresql.org/media/keys/ACCC4CF8.asc --output ACCC4CF8.asc
 sudo apt-key add ACCC4CF8.asc
-echo "deb http://apt.postgresql.org/pub/repos/apt/ 'lsb_release -cs'-pgdg main" | sudo tee  /etc/apt/sources.list.d/pgdg.list
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
 apt-get update
 sudo apt -y install postgresql-client-11
 mkdir backups
